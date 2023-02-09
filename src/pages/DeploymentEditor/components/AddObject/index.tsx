ae93e6131d8bde0c9bd3c0756fa0692f1eab3fe8
mertens <mertenslzm@gmail.com>
2023-02-10 01:48:06 +0800

    fix: react html

--- src/pages/DeploymentEditor/components/AddObject/index.tsx
+++ src/pages/DeploymentEditor/components/AddObject/index.tsx
@@ -125,8 +125,8 @@ export function AddObject(props: {
         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
         <g
           id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
           {" "}
