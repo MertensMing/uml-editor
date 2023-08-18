aa01ef3d2dc205a0dec57d43a30b3f038421595b
mertens <mertenslzm@gmail.com>
2023-08-19 02:06:22 +0800

    优化滚动条逻辑

--- src/shared/components/EditorLayout/index.tsx
+++ src/shared/components/EditorLayout/index.tsx
@@ -33,11 +33,23 @@ export function EditorLayout(props: {
         <div className="p-2 px-8 shadow-lg bg-white space-x-8 flex items-center h-12 absolute top-0 left-0 w-full z-10">
           {props.toolbar}
         </div>
-        <div className="overflow-auto flex w-screen">
-          <div className="bg-white flex-shrink-0 shadow-xl mt-20 mb-8 ml-8 mr-8 rounded p-4 scrollbar-thumb-slate-300 scrollbar-thin overflow-x-hidden overflow-y-auto">
-            {props.operation}
-          </div>
-          <div className="mr-8 pt-16 flex-grow">{props.diagram}</div>
+        <div
+          className="
+          bg-white flex-shrink-0 shadow-xl
+          mt-20 mb-8 ml-8 mr-8 rounded p-4
+          scrollbar-thumb-slate-300 scrollbar-thin
+          overflow-x-hidden overflow-y-auto
+          absolute left-0 top-0
+          "
+          style={{ height: 500 }}
+        >
+          {props.operation}
+        </div>
+        <div
+          className="w-screen pt-16 pb-16 box-border overflow-auto scrollbar-thumb-slate-300 scrollbar-thin"
+          style={{ paddingLeft: 270, paddingRight: 32 }}
+        >
+          {props.diagram}
         </div>
       </div>
     </div>
